0897918a8dc6a3650be8161b65b7569bf55c59cf
IMAC <imac@IMAC-iMac.local>
2018-06-23 12:27:30 +0300

    toml

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,14 @@ import MyCommentBox from './MyCommentBox.jsx';
 const fakeBlogPostId = 'my-blog-post';
 
 const postData = (url, data) => {
-
-    return fetch(`.netlify/functions${url}`, {
-        body: JSON.stringify(data),
-        headers: {
-            'content-type': 'application/json'
-        },
-        method: 'POST',
-        //mode: 'cors' // if your endpoints are on a different domain
-    }).then(response => response.json());
+  return fetch(`.netlify/functions${url}`, {
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST'
+    //mode: 'cors' // if your endpoints are on a different domain
+  }).then(response => response.json());
 };
 
 const contentfulClient = createClient({
@@ -26,18 +25,48 @@ const contentfulClient = createClient({
   host: process.env.REACT_APP_CONTENTFUL_HOST || 'preview.contentful.com'
 });
 
-const App = props => (
-    <div>
-        <h3>Authless Comments Example</h3>
-        <div>
-            <MyCommentBox
-                subjectId={fakeBlogPostId}
-                postData={postData}
-                contentfulClient={contentfulClient}
-            />
-        </div>
-    </div>
-);
+class LambdaDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: null };
+  }
 
+  handleClick = e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+  };
+
+  render() {
+    const { loading, msg } = this.state;
+
+    return (
+      <p>
+        <button onClick={this.handleClick}>
+          {loading ? 'Loading...' : 'Call Lambda'}
+        </button>
+        <br />
+        <span>{msg}</span>
+      </p>
+    );
+  }
+}
+
+const App = props => (
+  <div>
+    <h3>Authless Comments Example</h3>
+    <LambdaDemo />
+    <div>
+      <MyCommentBox
+        subjectId={fakeBlogPostId}
+        postData={postData}
+        contentfulClient={contentfulClient}
+      />
+    </div>
+  </div>
+);
 
 export default App;
